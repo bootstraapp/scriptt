6a2308c1c8ac84ee1099e43259afd2f9f100b364
bootstraapp <irfann46@gmail.com>
2024-05-06 09:22:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function overrideImportantStyle() {
-    var bodyElement = document.body;
-    bodyElement.style.setProperty('display', 'none', 'important');
+    // var bodyElement = document.body;
+    // bodyElement.style.setProperty('display', 'none', 'important');
     let crtDiv = document.getElementsByName(div)
     crtDiv.replace('div', 'span')
 }
